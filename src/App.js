1998e01308a58f7bbcecf36bd445950ef3fcbb2b
Bryce Rothhammer <rothhammer.b@gmail.com>
2021-02-09 23:58:21 -0600

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { workouts } from './assets/workouts';
+import {
+  Typography,
+  makeStyles,
+  Grid,
+  Paper,
+  Box,
+} from "@material-ui/core";
+import TimerIcon from '@material-ui/icons/Timer';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+
 
 function App() {
+  const [isShown, setIsShown] = useState(null);
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App"
+           style={{
+             display: "flex",
+             justifyContent: "center",
+             alignItems: "center",
+             paddingTop: "40px"
+           }}>
+        <Box justify="center" width='80%'>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            {workouts.map((tile) => (
+              <Grid
+                item xs={3}
+                className={classes.gridItem}
+                onMouseOver={() => setIsShown(tile.id)}
+                onMouseOut={() => setIsShown(null)}
+              >
+              <Paper elevation={5} variant="outlined" >
+                <div className={classes.container}>
+                  <img
+                    src={require(`./assets/images/${tile.name}-thumb.jpg`)}
+                    alt={tile.name}
+                    className={classes.topImg}
+                  />
+                  ${tile.count && <div className={classes.textRight}>
+                  <h4 className={classes.workoutCount}>{tile.count}</h4>
+                  <Typography>workouts</Typography>
+                  <PlaylistPlayIcon/>
+                </div>
+                }
+                </div>
+                  <Grid container spacing={0}>
+                    <Grid item xs={8}>
+                      <h5 className={classes.workoutHeading}>{tile.title}</h5>
+                    </Grid>
+                    <Grid item xs={2}>
+                    <img
+                      src={require(`./assets/images/${tile.name}-trainer.jpg`)}
+                      alt={tile.name}
+                      className={classes.smallImg}
+                    />
+                    </Grid>
+                    <Grid item>
+                      {tile.time && tile.distance &&
+                      <Typography className={classes.text}>
+                       <TimerIcon className={classes.iconPadding}/>{tile.time}
+                        <DirectionsRunIcon className={classes.iconPadding}/> {tile.distance}
+                      </Typography>}
+                        <Typography className={classes.text} style={{color: 'blue'}} >
+                          {isShown === tile.id ? 'View Details' : ''}
+                        </Typography>
+                    </Grid>
+                  </Grid>
+              </Paper>
+              </Grid>
+              )
+            )}
+        </Grid>
+        </Box>
+      </div>
+    </>
   );
 }
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    textAlign: 'center',
+  },
+  topImg: {
+    width: '100%',
+    height: '50%',
+    },
+  smallImg: {
+    width: 40,
+    height: 40,
+    paddingTop: '20px',
+  },
+  iconPadding: {
+    paddingLeft: '1px',
+    paddingRight: '1px',
+    fontSize: 12,
+  },
+  text: {
+    fontSize: 12,
+  },
+  workoutHeading: {
+    marginBottom: '2px',
+  },
+  workoutCount: {
+    marginBottom: '8px',
+  },
+  gridItem: {
+    height: 240,
+  },
+  textRight: {
+    position: 'absolute',
+    top: '0px',
+    right: '0px',
+    background: '#000000',
+    opacity: 0.5,
+},
+  container: {
+    position: 'relative',
+    textAlign: 'center',
+    color: 'white',
+}
+}));
 
 export default App;
---
+++ src/assets/workouts.js
@@ -0,0 +1,54 @@
+export const workouts = [
+	{
+		id: 0,
+		name: 'lake-inniscarra',
+		title: 'Lake Inniscarra, Ireland-Pyramid',
+		time: '30:53',
+		distance: '6,248 M',
+	},
+	{
+		id: 1,
+		name: 'performance-series',
+		title: 'Performance Series',
+		count: 9,
+	},
+	{
+		id: 2,
+		name: 'slow-pulls',
+		title: 'Slow Pulls and Fast Intervals',
+		time: '44:13',
+		distance: '9,948 M',
+	},
+	{
+		id: 3,
+		name: '20-minutes-to-toned',
+		title: '20 Minutes to Toned',
+		count: 12,
+	},
+	{
+		id: 4,
+		name: 'charles-race',
+		title: 'Charles Race, Boston, Massachusetts',
+		time: '36:22',
+		distance: '8,648 M',
+	},
+	{
+		id: 5,
+		name: 'full-body-hiit',
+		title: 'Full-Body HIIT Series',
+		count: 12,
+	},
+	{
+		id: 6,
+		name: 'kafue-river',
+		title: 'Kafue River, Zambia—Power Stroke Pyramid',
+		time: "22:22",
+		distance: "4,660 M",
+	},
+	{
+		id: 7,
+		name: 'shred-and-burn',
+		title: 'Shred & Burn Series',
+		count: 16,
+	},
+]
